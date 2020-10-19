031652221caa14829df225733b5380f6b27b5b33
chip <onlygamernerd@gmail.com>
2020-10-19 19:56:00 +0200

    Initial commit - Vue listings

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header msg="Hello" />
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Header/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">{{ msg }}</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+.header {
+  position: absolute;
+  background: #232323;
+  color: #fff;
+  width: 100%;
+  left: 0;
+  top: 0;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+}
+
+</style>
